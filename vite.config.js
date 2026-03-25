903ab09b2654db75a5aefa56f7090af8e006a414
smkrispin <smkrispin@gmail.com>
2026-03-24 23:11:57 -0400

    fix: update base path for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,12 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
+  base: '/gridfinity-app/',
   // This tells Vite to build the worker as a modern ES Module
   // which prevents it from trying to use 'window' or 'document'
   worker: {
     format: 'es',
   },
-
   build: {
     outDir: 'build', // Ensures it matches your Docker COPY command
     assetsInlineLimit: 0, // Keeps WASM and Worker as separate files
