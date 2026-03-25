7ac2dd794df5dd24bc496b56766d5ba39db7b26c
smkrispin <smkrispin@gmail.com>
2026-03-24 23:25:01 -0400

    Updates

--- src/cad.js
+++
@@ -1,8 +0,0 @@
-import { drawRoundedRectangle } from "replicad";
-
-// The replicad code! Not much there!
-export function drawBox(thickness, width) {
-  return drawRoundedRectangle(30, 50, 4)
-    .sketchOnPlane()
-    .extrude(thickness * 7)
-}
